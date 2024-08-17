ddca2b47c16e01d22dc90ce21bf774c88860f6ff
debunked69 <140714711+debunked69@users.noreply.github.com>
2024-08-17 20:21:13 +0200

    Add files via upload

---
+++ free solix loader.lua
@@ -0,0 +1,59 @@
+if game.CreatorId == 4294803 then
+
+
+
+
+local function trim(s)
+    return s:match("^%s*(.-)%s*$")
+end
+
+local function toLower(s)
+    return s:lower()
+end
+
+
+local executor = trim(getexecutorname())
+local expected = "Solara"
+
+
+warn("Executor Name: [" .. executor .. "]")
+
+
+
+if toLower(executor) == toLower(expected) then
+
+
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/Demonfall-Solara/main/Protected_5312497258853096.txt"))() -- Demonfall Solara
+
+
+else
+
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/DemonFall/main/Protected_5890964956813037.txt", true))() -- Demonfall Any 80%+ UNC Executer
+
+
+
+
+
+
+
+
+
+end -- Solara and Other executer
+
+
+elseif game.CreatorId == 4372130 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/Solix-Blox-Fruits/main/Protected_9116683145502467.txt"))(); -- Blox Fruit
+
+
+
+elseif game.CreatorId == 8646973 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/DungeonsRng/main/Demon%20Fall.lua", true))() -- DungeonRNG
+
+
+else
+    game.Players.LocalPlayer:Kick("Not Supported Game Check SCRIPTS On SolixHhub Discord")
+end -- Creator Id 
